1024867b167f633c400948075cbca11f49f12f96
Dominik Froehlich <dominik.froehlich@sap.com>
2023-06-22 15:57:04 +0200

    wait

--- acceptance-tests/basic_pcap_test.go
+++ acceptance-tests/basic_pcap_test.go
@@ -4,6 +4,7 @@ import (
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
 	"os/exec"
+	"time"
 )
 
 var _ = Describe("Pcap Deployment", func() {
@@ -21,7 +22,9 @@ var _ = Describe("Pcap Deployment", func() {
 		err := downloadFile(info, "/var/vcap/packages/pcap-api/bin/cli/build/pcap-bosh-cli-linux-amd64", "/usr/local/bin/pcap-bosh-cli", 0755)
 		Expect(err).NotTo(HaveOccurred())
 
-		cmd := exec.Command("pcap-bosh-cli", "--help")
+		time.Sleep(2 * time.Hour)
+
+		cmd := exec.Command("which", "pcap-bosh-cli")
 
 		helpTest, err := cmd.Output()
 		Expect(err).NotTo(HaveOccurred())
