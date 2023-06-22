f5721531eb296cac55c6e8c8f574afda248bad5c
Dominik Froehlich <dominik.froehlich@sap.com>
2023-06-22 14:59:04 +0200

    separate manifest for agent

--- acceptance-tests/basic_pcap_test.go
+++ acceptance-tests/basic_pcap_test.go
@@ -21,7 +21,12 @@ var _ = Describe("Pcap Deployment", func() {
 		err := downloadFile(info, "/var/vcap/packages/pcap-api/bin/cli/build/pcap-bosh-cli-linux-amd64", "/usr/local/bin/pcap-bosh-cli", 0755)
 		Expect(err).NotTo(HaveOccurred())
 
-		exec.Command("pcap-bosh-cli", "--help")
+		cmd := exec.Command("pcap-bosh-cli", "--help")
+
+		helpTest, err := cmd.Output()
+		Expect(err).NotTo(HaveOccurred())
+
+		GinkgoLogr.Info(string(helpTest))
 
 	})
 })
